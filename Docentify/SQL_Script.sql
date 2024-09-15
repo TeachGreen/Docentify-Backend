26bd096e0c7b6f2084fc841f712304ba904df2a7
PedroTejon <pedrohtejon@gmail.com>
2024-09-14 22:12:43 -0300

    docs: SQL_Script.sql atualizado para remover indexes explícitas desnecessárias

--- Docentify/SQL_Script.sql
+++ Docentify/SQL_Script.sql
@@ -19,10 +19,8 @@ CREATE TABLE IF NOT EXISTS Users
     isInstitution TINYINT      NOT NULL DEFAULT 0,
     institutionId INT          NOT NULL,
     PRIMARY KEY (userId),
-    INDEX fk_User_Organization_idx (institutionId ASC) VISIBLE,
-    CONSTRAINT fk_User_Organization
-        FOREIGN KEY (institutionId)
-            REFERENCES Institutions (institutionId)
+    FOREIGN KEY (institutionId)
+        REFERENCES Institutions (institutionId)
 );
 
 CREATE TABLE IF NOT EXISTS PasswordHashes
@@ -31,10 +29,8 @@ CREATE TABLE IF NOT EXISTS PasswordHashes
     salt           INT NULL,
     userId         INT NOT NULL,
     PRIMARY KEY (hashedPassword),
-    INDEX fk_PasswordHashes_User1_idx (userId ASC) VISIBLE,
-    CONSTRAINT fk_PasswordHashes_User1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId)
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId)
 );
 
 CREATE TABLE IF NOT EXISTS Courses
@@ -43,26 +39,20 @@ CREATE TABLE IF NOT EXISTS Courses
     name          VARCHAR(45) NOT NULL,
     institutionId INT         NOT NULL,
     PRIMARY KEY (course_id),
-    INDEX fk_Courses_Organization1_idx (institutionId ASC) VISIBLE,
-    CONSTRAINT fk_Courses_Organization1
-        FOREIGN KEY (institutionId)
-            REFERENCES Institutions (institutionId)
+    FOREIGN KEY (institutionId)
+        REFERENCES Institutions (institutionId)
 );
 
-CREATE TABLE IF NOT EXISTS FavoritedCourses
+CREATE TABLE IF NOT EXISTS FavoriteCourses
 (
     courseId     INT      NOT NULL,
     userId       INT      NOT NULL,
     favoriteDate DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (courseId, userId),
-    INDEX fk_Courses_has_User_User1_idx (userId ASC) VISIBLE,
-    INDEX fk_Courses_has_User_Courses1_idx (courseId ASC) VISIBLE,
-    CONSTRAINT fk_Courses_has_User_Courses1
-        FOREIGN KEY (courseId)
-            REFERENCES Courses (course_id),
-    CONSTRAINT fk_Courses_has_User_User1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId)
+    FOREIGN KEY (courseId)
+        REFERENCES Courses (course_id),
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId)
 );
 
 CREATE TABLE IF NOT EXISTS Enrollments
@@ -73,15 +63,11 @@ CREATE TABLE IF NOT EXISTS Enrollments
     enrollmentDate DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
     isRequired     TINYINT  NULL DEFAULT 0,
     requiredDate   DATETIME NULL,
-    INDEX fk_User_has_Courses_User1_idx (userId ASC) VISIBLE,
-    INDEX fk_Enrollment_Courses1_idx (courseId ASC) VISIBLE,
     PRIMARY KEY (enrollmentId),
-    CONSTRAINT fk_User_has_Courses_User1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId),
-    CONSTRAINT fk_Enrollment_Courses1
-        FOREIGN KEY (courseId)
-            REFERENCES Courses (course_id)
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId),
+    FOREIGN KEY (courseId)
+        REFERENCES Courses (course_id)
 );
 
 CREATE TABLE IF NOT EXISTS Steps
@@ -90,10 +76,8 @@ CREATE TABLE IF NOT EXISTS Steps
     stepName VARCHAR(45) NOT NULL,
     courseId INT         NOT NULL,
     PRIMARY KEY (stepId),
-    INDEX fk_Steps_Courses1_idx (courseId ASC) VISIBLE,
-    CONSTRAINT fk_Steps_Courses1
-        FOREIGN KEY (courseId)
-            REFERENCES Courses (course_id)
+    FOREIGN KEY (courseId)
+        REFERENCES Courses (course_id)
 );
 
 CREATE TABLE IF NOT EXISTS VideoSteps
@@ -102,10 +86,8 @@ CREATE TABLE IF NOT EXISTS VideoSteps
     videoUrl    VARCHAR(250) NOT NULL,
     stepId      INT          NOT NULL,
     PRIMARY KEY (videoStepId, stepId),
-    INDEX fk_VideoStep_Steps1_idx (stepId ASC) VISIBLE,
-    CONSTRAINT fk_VideoStep_Steps1
-        FOREIGN KEY (stepId)
-            REFERENCES Steps (stepId)
+    FOREIGN KEY (stepId)
+        REFERENCES Steps (stepId)
 );
 
 CREATE TABLE IF NOT EXISTS Activities
@@ -113,10 +95,8 @@ CREATE TABLE IF NOT EXISTS Activities
     activityId INT NOT NULL AUTO_INCREMENT,
     stepId     INT NOT NULL,
     PRIMARY KEY (activityId, stepId),
-    INDEX fk_Activities_Steps1_idx (stepId ASC) VISIBLE,
-    CONSTRAINT fk_Activities_Steps1
-        FOREIGN KEY (stepId)
-            REFERENCES Steps (stepId)
+    FOREIGN KEY (stepId)
+        REFERENCES Steps (stepId)
 );
 
 CREATE TABLE IF NOT EXISTS UserProgress
@@ -126,18 +106,12 @@ CREATE TABLE IF NOT EXISTS UserProgress
     stepId       INT      NOT NULL,
     progressDate DATETIME NOT NULL,
     PRIMARY KEY (userId, courseIid),
-    INDEX fk_UserProgress_Users1_idx (userId ASC) VISIBLE,
-    INDEX fk_UserProgress_Courses1_idx (courseIid ASC) VISIBLE,
-    INDEX fk_UserProgress_Steps1_idx (stepId ASC) VISIBLE,
-    CONSTRAINT fk_UserProgress_Users1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId),
-    CONSTRAINT fk_UserProgress_Courses1
-        FOREIGN KEY (courseIid)
-            REFERENCES Courses (course_id),
-    CONSTRAINT fk_UserProgress_Steps1
-        FOREIGN KEY (stepId)
-            REFERENCES Steps (stepId)
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId),
+    FOREIGN KEY (courseIid)
+        REFERENCES Courses (course_id),
+    FOREIGN KEY (stepId)
+        REFERENCES Steps (stepId)
 );
 
 CREATE TABLE IF NOT EXISTS Questions
@@ -146,10 +120,8 @@ CREATE TABLE IF NOT EXISTS Questions
     statement  TEXT NOT NULL,
     activityId INT  NOT NULL,
     PRIMARY KEY (questionId, activityId),
-    INDEX fk_Questions_Activities1_idx (activityId ASC) VISIBLE,
-    CONSTRAINT fk_Questions_Activities1
-        FOREIGN KEY (activityId)
-            REFERENCES Activities (activityId)
+    FOREIGN KEY (activityId)
+        REFERENCES Activities (activityId)
 );
 
 CREATE TABLE IF NOT EXISTS Options
@@ -159,10 +131,8 @@ CREATE TABLE IF NOT EXISTS Options
     isCorrect  TINYINT NULL DEFAULT 0,
     questionId INT     NOT NULL,
     PRIMARY KEY (answerIid, questionId),
-    INDEX fk_Options_Questions1_idx (questionId ASC) VISIBLE,
-    CONSTRAINT fk_Options_Questions1
-        FOREIGN KEY (questionId)
-            REFERENCES Questions (questionId)
+    FOREIGN KEY (questionId)
+        REFERENCES Questions (questionId)
 );
 
 CREATE TABLE IF NOT EXISTS FileSteps
@@ -171,10 +141,8 @@ CREATE TABLE IF NOT EXISTS FileSteps
     fileData   BLOB NOT NULL,
     stepId     INT  NOT NULL,
     PRIMARY KEY (fileStepId, stepId),
-    INDEX fk_VideoStep_Steps1_idx (stepId ASC) VISIBLE,
-    CONSTRAINT fk_VideoStep_Steps10
-        FOREIGN KEY (stepId)
-            REFERENCES Steps (stepId)
+    FOREIGN KEY (stepId)
+        REFERENCES Steps (stepId)
 );
 
 CREATE TABLE IF NOT EXISTS UserScores
@@ -182,10 +150,8 @@ CREATE TABLE IF NOT EXISTS UserScores
     userId INT NOT NULL,
     score  INT NULL DEFAULT 0,
     PRIMARY KEY (userId),
-    INDEX fk_Users_copy1_Users1_idx (userId ASC) VISIBLE,
-    CONSTRAINT fk_Users_copy1_Users1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId)
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId)
 );
 
 CREATE TABLE IF NOT EXISTS CourseStyles
@@ -195,10 +161,8 @@ CREATE TABLE IF NOT EXISTS CourseStyles
     pageId   VARCHAR(45) NOT NULL,
     style    LONGTEXT    NULL,
     PRIMARY KEY (styleId, pageId),
-    INDEX fk_CourseStyles_Courses1_idx (courseId ASC) VISIBLE,
-    CONSTRAINT fk_CourseStyles_Courses1
-        FOREIGN KEY (courseId)
-            REFERENCES Courses (course_id)
+    FOREIGN KEY (courseId)
+        REFERENCES Courses (course_id)
 );
 
 CREATE TABLE IF NOT EXISTS UserPreferences
@@ -214,12 +178,8 @@ CREATE TABLE IF NOT EXISTS UserPreferencesValues
     preferenceName VARCHAR(50) NOT NULL,
     value          VARCHAR(45) NOT NULL,
     PRIMARY KEY (userId, preferenceName),
-    INDEX fk_Users_has_UserPreferences_UserPreferences1_idx (preferenceName ASC) VISIBLE,
-    INDEX fk_Users_has_UserPreferences_Users1_idx (userId ASC) VISIBLE,
-    CONSTRAINT fk_Users_has_UserPreferences_Users1
-        FOREIGN KEY (userId)
-            REFERENCES Users (userId),
-    CONSTRAINT fk_Users_has_UserPreferences_UserPreferences1
-        FOREIGN KEY (preferenceName)
-            REFERENCES UserPreferences (preferenceName)
+    FOREIGN KEY (userId)
+        REFERENCES Users (userId),
+    FOREIGN KEY (preferenceName)
+        REFERENCES UserPreferences (preferenceName)
 );
